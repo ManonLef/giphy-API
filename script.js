e32305e102b50267113f896d1d46c13c4f2d8a24
Manon <manonlefkos@gmail.com>
2023-05-19 16:38:18 +0200

    add button to change random image

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const img = document.querySelector("img");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=the_office",
+function getImage() {
+  fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cats",
   { mode: "cors" }
 )
   .then(function (response) {
@@ -10,3 +11,8 @@ fetch(
   .then(function (response) {
     img.src = response.data.images.original.url;
   });
+}
+const button = document.querySelector("button")
+
+getImage()
+button.addEventListener("click", getImage)
