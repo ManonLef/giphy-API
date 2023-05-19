8b0ac820e36b2b41d0896613d110ec75ee1c06a9
Manon <manonlefkos@gmail.com>
2023-05-19 16:40:58 +0200

    change back to cats instead of office for more gif diversity

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const img = document.querySelector("img");
 
 function getImage() {
   fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cats",
+  "https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=cat",
   { mode: "cors" }
 )
   .then(function (response) {
