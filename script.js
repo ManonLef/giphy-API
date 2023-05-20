e5c84498888c2c4b9c3de4e6238746b0874c9765
Manon <manonlefkos@gmail.com>
2023-05-21 00:13:03 +0200

    add catch for undefined search

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ function getImage(searchTerm) {
   { mode: "cors" }
 )
   .then(function (response) {
+    console.log("https://api.giphy.com/v1/gifs/translate?api_key=8PltmVYYZ2B7WBUvMceFkHZTT2dC0jsq&s=" + searchTerm);
     return response.json();
   })
   .then(function (response) {
     img.src = response.data.images.original.url;
-  });
+  })
+  .catch(function () {
+    if (searchTerm === "") {
+      getImage("404")
+    }
+  })
 }
-const button = document.querySelector("button")
+const button = document.querySelector(".new")
 
 getImage()
 button.addEventListener("click", getImage)
@@ -23,7 +29,8 @@ const searchButton = document.querySelector(".search")
 
 searchButton.addEventListener("click", search)
 
-function search() {
+function search(event) {
+  event.preventDefault();
   const search = searchInput.value
   getImage(search)
 }
